01367d508adc7133f001b1ab2185855f54152b1f
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-10 10:38:01 +0530

    valu added

--- customer.py
+++ customer.py
@@ -1,3 +1,10 @@
+import mysql.connector
+import sys
+try:
+    mydb=mysql.connector.connect(host='localhost',user='root',password='',database='admindb')
+except mysql.connector.Error as e:  
+    sys.exit("dbconnection failure")    
+mycursor=mydb.cursor()
 while True:
     print("select an option from the menu")
     print("--------------------------------")
@@ -12,6 +19,19 @@ while True:
     choice=int(input("enter your choice:-"))
     if(choice==1):
         print("add customer")
+        code=input("enter a code:::---")
+        name=input("enter name:::---")
+        address=input("enter address:::---")
+        phone=input("enter phone:::---")
+        email=input("enter email:::---")
+        try:
+            sql="INSERT INTO `customer`(`code`, `name`, `address`, `phone`, `email`) VALUES (%s,%s,%s,%s,%s)"
+            data=(code,name, address,phone,email)
+            mycursor.execute(sql,data)
+            mydb.commit()
+            print("values entared successfully.......!")
+        except mysql.connector.Error as e:
+            sys.exit("view date error")
     elif(choice==2):
         print("search customer")
     elif(choice==3):
