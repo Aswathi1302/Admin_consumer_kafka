19a6642c6c14d717b1fdf358993a0872d45379ea
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-10 10:27:23 +0530

    first commit

---
+++ customer.py
@@ -0,0 +1,26 @@
+while True:
+    print("select an option from the menu")
+    print("--------------------------------")
+    print("1. add customer")
+    print("2.search customer")
+    print("3.delete customer")
+    print("4.upadte customer")
+    print("5. view all customer")
+    print("6.exit")
+
+    print("--------------------------------")
+    choice=int(input("enter your choice:-"))
+    if(choice==1):
+        print("add customer")
+    elif(choice==2):
+        print("search customer")
+    elif(choice==3):
+        print("delete customer")   
+    elif(choice==4):
+        print("delete customer")  
+    elif(choice==5):
+        print("delete customer")  
+    elif(choice==6):
+        break
+
+                
