10656cdc144fe433e4696db9b681e90f82723545
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-10 16:52:57 +0530

    due date added

--- customer.py
+++ customer.py
@@ -120,7 +120,7 @@ while True:
         #total_bill = int(result)*5
             total_bill=int(str(result[0])) * 5
             print(total_bill)
-            sql="INSERT INTO `bill`(`userid`, `month`, `year`, `bill`, `paidstatus`, `billdate`, `totalunit`) VALUES (%s,%s,%s,%s,%s,now(),%s)"
+            sql="INSERT INTO `bill`(`userid`, `month`, `year`, `bill`, `paidstatus`, `billdate`, `totalunit`,`duedate`) VALUES (%s,%s,%s,%s,%s,now(),%s,now()+interval 14 day)"
             data = (str(a),str(month),str(year),total_bill,'0',unit)
 
             mycursor.execute(sql,data)
